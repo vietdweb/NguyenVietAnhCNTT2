2cd7d431ff67a0613daf1b6bf2d0b5c839f5d055
Viet Nguyen <rtmx24@gmail.com>
2026-09-12 19:28:51 +0700

    1

--- BVN4-BTTl/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BVN4_BTTl.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
